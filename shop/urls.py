dc0c811508a2f89433d55df6bfc5a7ebcba9cd0f
zhakypova <zulyawayne@gmail.com>
2022-11-07 12:55:55 +0600

    second hw

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import greetings, list_item, detail_item
+from .views import greetings, ListItem, Detail
 
 
 urlpatterns = [
     path('greetings/', greetings, name='greetings'),
-    path('shop/', list_item, name = 'list_item'),
-    path('shop/<int:item_id>/', detail_item, name='detail_item'),
+    path('shop/', ListItem.as_view(), name = 'list_item'),
+    path('shop/<int:pk>/', Detail.as_view(), name='detail_item'),
 ]
 
 
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Item, Purchase
+
 
 def greetings(request):
     return HttpResponse("Добро пожаловать в наш магазин!")
 
-def list_item(request):
-    items = Item.objects.all()
-    context = {
-        'items' : items,
-    }
-    return render(request, 'list_item.html', context)
+class ListItem(generic.ListView):
+    model = Item
+    template_name = 'list_item.html'
+    context_object_name = 'items'
 
-def detail_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    context = {
-        'item':item,
-    }
-    return render(request, 'detail_item.html', context)
+class Detail(generic.DetailView):
+    model = Item
+    template_name = 'detail_item.html'
+    context_object_name = 'item'
